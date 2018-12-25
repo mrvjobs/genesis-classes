4585b563ec678413402219d37446dfc237c0f330
Ruslan_M <mrvjobs@gmail.com>
2018-12-25 20:09:04 +0300

    ДЗ№23

---
+++ Занятие 23/scripts.js
@@ -0,0 +1,32 @@
+/* Задание №1*/
+function HelloWorld(name) {
+    return "Hello, " + name;
+}
+
+/* Задание #2*/
+function calculateDifference(arg1, arg2, arg3){
+    return arg1 - arg2 - arg3;
+}
+
+/* Задание #3*/
+function dayOrNight(arg){
+    if arg(=== 1){
+        return "день";
+    }
+
+    if arg(=== 2){
+        return "ночь";
+    }
+}
+/*Задание #4*/
+function isResultValid(firstNumber, secondNumber, result) {
+    return firstNumber + secondNumber === result;
+}
+
+/* Задание #5*/
+
+function HelloWorldFiveTimes() {
+    for (var i = 0; i<5; i++){
+        console.log("Hello, World");
+    }
+}
